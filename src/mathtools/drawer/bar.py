bcd09a02820338f56a74156a4ac91c51ada2a89a
mulimuri <mulimuri@outlook.com>
2024-07-20 22:20:19 +0800

    Re pep8

---
+++ src/mathtools/drawer/bar.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@File    :   bar.py
+@Time    :   2024/07/20 21:02:13
+@Author  :   MuliMuri
+@Version :   1.0
+@Desc    :   Bar drawer class
+'''
