b260a582c8103646fc429cfa248af54f279f3d55
mulimuri <mulimuri@outlook.com>
2024-08-25 11:43:56 +0800

    clean drawer before PR

--- src/mathtools/drawer/bar.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-'''
-@File    :   bar.py
-@Time    :   2024/07/20 21:02:13
-@Author  :   MuliMuri
-@Version :   1.0
-@Desc    :   Bar drawer class
-'''
